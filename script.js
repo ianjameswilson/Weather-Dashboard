af78e92d80698afb5bad9e8b0e330cf1f90fc813
ianjameswilson <60559809+ianjameswilson@users.noreply.github.com>
2020-07-21 16:49:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,32 @@
 $(document).ready(function(){
+    
+    // add click function to button
+    $('#find-city').click(function() {
 
-    function displayCityInfo() {
-        
-        var city = $("#city-input").val().trim();
-        console.log(city);
+        var city = $("#city-input").val();
 
-        var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1d140af8cbf6d4a0a9813769986f988f" 
-        console.log(queryURL);
+        // AJAX call for current weather
+        $.ajax ({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=1d140af8cbf6d4a0a9813769986f988f',
+            method: "GET",
+            success: function(data){
+                var widget = displayCityInfo(data);
 
-        // Creating an AJAX call for the city being searched
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
+                $("#display-data").text(JSON.stringify(widget));
 
-            // Creating a div to hold the city name
-            var cityDiv = $("<div class='name'>");
-
-            // Storing the city name
-            var cityName = response.name.value;
-
-            // Display the city name
-            cityDiv.append(cityName);
-            
-            // Storing the temperature
-            var temperature = response.temperature.value;
-            console.log(temperature);
-
-            // Creating an element to have the temperature displayed
-            var pOne = $("<p>").text("Temperature: " + temperature);
-
-            // Display the temperature
-            cityDiv.append(pOne);
-
-            // Storing the humidity
-            var humidity = response.main.humidity.value;
-            console.log(humidity);
-
-            // Creating an element to have the humidity displayed
-            var pTwo = $("<p>").text("Humidity: " + humidity);
-
-            // Display the humidity
-            cityDiv.append(pTwo);
-
-            // Storing the wind speed
-            var windSpeed = response.wind.speed.value;
-            console.log(windSpeed);
-
-            // Creating an element to have the wind speed displayed
-            var pThree = $("<p>").text("Wind Speed: " + windSpeed);
-
-            // Display the wind speed
-            cityDiv.append(pThree);
+                $("#city-input").val('');
+            }
         });
-    };
+    });
 
-    // This function handles events when submit button is clicked
-    $("#find-city").on("click", function(event) {
-        event.preventDefault();
-        
-        // Call displayCityInfo function
-        displayCityInfo();
-    };    
-});
-    
+    // display data parameters 
+    function displayCityInfo(data){
+        return  data.name + moment().format('MMMM Do YYYY') +
+                "Weather: "+ data.weather[0].main +"" +
+                "Description: "+ data.weather[0].description +"" +
+                "Temperature: "+ data.main.temp +" °F" +
+                "Humidity: "+ data.main.humidity +" %" +
+                "Wind Speed: "+ data.wind.speed +" mph";
+        };
+    });
 
-
-
-
-    
-
-     
